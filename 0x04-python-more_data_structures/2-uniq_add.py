62c6fc494ec0f6e29769768267f020a198154de4
Ruth Njeri <rthmbugua@gmail.com>
2021-08-23 10:49:47 +0000

    print number of keys in a dict

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,6 @@ def uniq_add(my_list=[]):
     new_list = []
     for item in my_list:
         if item not in new_list:
-           new_list.append(item)
-           sum += item
+            new_list.append(item)
+            sum += item
     return sum
