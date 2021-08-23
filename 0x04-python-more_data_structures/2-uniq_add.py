2ed28dfab56585ee1971e47b4b1ff38c0379007a
Ruth Njeri <rthmbugua@gmail.com>
2021-08-23 10:38:20 +0000

    add function for set difference

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,7 @@ def uniq_add(my_list=[]):
     sum = 0
     new_list = []
     for item in my_list:
-        if item in new_list:
-            print('item already exit')
-        else:
-            new_list.append(item)
-            sum += item
+        if item not in new_list:
+           new_list.append(item)
+           sum += item
     return sum
