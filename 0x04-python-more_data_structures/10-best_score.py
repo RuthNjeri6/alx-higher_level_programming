598d4c07ed5653b2392e15b2e2820e9171006813
Ruth Njeri <rthmbugua@gmail.com>
2021-08-24 18:53:39 +0000

    add function to multipy list items using map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     biggest = 0
     try:
+        if len(a_dictionary) == 0:
+            return None
         for key, value in a_dictionary.items():
             if value >= biggest:
                 biggest = value
