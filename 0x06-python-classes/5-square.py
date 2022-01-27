b1efac67ce7c86ccb7976caacae73c2343c84067
RuthNjeri6 <rthmbugua@gmail.com>
2022-01-27 18:49:58 +0000

    class square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,9 +25,9 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print('\n')
+            print('')
         else:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
                     print('#', end="")
-                print('\n')
+                print('')
