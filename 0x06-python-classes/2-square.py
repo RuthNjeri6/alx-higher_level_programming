1d227d761ecfe1af812fdfd87d54795d86dd2c34
RuthNjeri6 <rthmbugua@gmail.com>
2022-01-27 05:56:15 +0000

    add class square with attribute validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a class Square and validate the private instance attribute"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """Class with private instance attribute that is validated"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
