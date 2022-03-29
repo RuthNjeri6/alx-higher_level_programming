b7bdbd751e8168a4b3f29a5d32274d4ec2556348
RuthNjeri6 <rthmbugua@gmail.com>
2022-03-29 18:43:39 +0000

    add condition to check for float overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,11 +3,21 @@
 
 
 def add_integer(a, b=98):
-    """ Function that adds 2 numbers """
+    """Function that adds 2 numbers"""
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
+    try:
+        a = int(a)
+        if not float('-inf') < float(a) < float('inf'):
+            raise TypeError('a must be an integer')
+    except Exception as err:
+        raise TypeError('a must be an integer')
+    try:
+        b = int(b)
+        if not float('-inf') < float(a) < float('inf'):
+            raise TypeError('a must be an integer')
+    except Exception as err:
+        raise TypeError('b must be an integer')
     return (a + b)
